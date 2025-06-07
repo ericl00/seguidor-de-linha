2e69b4283a9ca7daa3be33f735ecbcdb76a02281
Eric Munaro <ericmunaro@hotmail.com>
2025-06-07 17:31:09 -0300

    Reorganização e ajustes no retorno de informação

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![no_main]
 
+use alloc::{
+    string::ToString,
+    format
+};
+use embedded_io::Write;
 use esp_hal::{
     main,
     clock::CpuClock,
@@ -13,9 +18,13 @@ use esp_hal::{
         LowSpeed,
         timer::TimerIFace,
     },
-    time::Rate,
+    time::{
+        Rate,
+        Instant,
+    },
     timer::timg::TimerGroup,
     analog::adc::{self, Attenuation},
+    system::software_reset,
 };
 use esp_wifi::{
     self,
@@ -26,7 +35,8 @@ use smoltcp::{
     iface::{
         Interface,
         SocketSet,
-        SocketStorage
+        SocketStorage,
+        SocketHandle
     },
     socket::{
             tcp::{
@@ -34,7 +44,7 @@ use smoltcp::{
             SocketBuffer
         },
     },
-    time::Instant,
+    time::Instant as SmoltcpInstant,
     wire::{
         EthernetAddress,
         HardwareAddress,
@@ -54,15 +64,27 @@ const RX_BUFFER_SIZE: usize = 1024;
 const TX_BUFFER_SIZE: usize = 1024;
 const NUM_SOCKETS: usize = 1;
 
+#[derive(PartialEq, Copy, Clone)]
+enum Command {
+    Start,
+    Stop,
+}
+
+struct Telemetria {
+    pwm_dir: u8,
+    pwm_esq: u8
+}
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     println!("panic!: {:?}", info);
-    loop {}
+    Delay::new().delay_millis(1000);
+    software_reset();
 }
 
 extern crate alloc;
 
+/// Calcula o desvio com base em 3 valores de entrada
 fn find_deviation(esquerda: u16, centro: u16, direita: u16) -> f32 {
     let valor_sensores = esquerda + centro + direita;
 
@@ -71,6 +93,9 @@ fn find_deviation(esquerda: u16, centro: u16, direita: u16) -> f32 {
     desvio
 }
 
+/// Calcula o PWM para retornar aos motores com base em um 
+/// desvio inserido na chamada da função
+/// retorna dois valores de 0 - 100 
 fn calc_pwm(desvio: f32) -> (u8, u8) {
     if desvio == 0.0 { return (100, 100) }
     else if desvio < 0.0 {
@@ -83,16 +108,74 @@ fn calc_pwm(desvio: f32) -> (u8, u8) {
     (0, 0)
 }
 
+/// Pega os handlers necessários e retorna o comando que o cliente responder,
+/// Envia dados de telemetria
+fn get_commands(
+    interface: &mut Interface,
+    socket_set: &mut SocketSet,
+    tcp_handle: &SocketHandle,
+    wifi_interfaces: &mut esp_wifi::wifi::Interfaces,
+    instant: &Instant,
+    motores: &Telemetria,
+) -> Option<Command> {
+    let timestamp = SmoltcpInstant::from_millis(instant.duration_since_epoch().as_millis() as i64);
+    interface.poll(timestamp, &mut wifi_interfaces.ap, socket_set);
+
+    let mut socket = socket_set.get_mut::<TcpSocket>(*tcp_handle);
+
+    let mut command = None;
+
+    if !socket.is_open() {
+        socket.listen(8080).unwrap();
+        println!("socket listening");
+    }
+
+    if socket.may_recv() {
+        let mut text = heapless::String::<32>::new();
+        let data = socket.recv(|recv| {
+            if !recv.is_empty() {
+                println!("tcp:8080 recv data: {:?}", recv);
+            }
+            let mut data = recv.to_vec();
+            (data.len(), data)
+        }).unwrap();
+        if !data.is_empty() {
+            text = heapless::String::from_utf8(heapless::Vec::from_slice(data.as_slice()).unwrap()).unwrap();
+            println!("tcp:8080 text: {:?}", text);
+        }
+        if text == "Start" {
+            command = Some(Command::Start);
+        } else if text == "Stop" {
+            command = Some(Command::Stop);
+        }
+    }
+    if socket.can_send() {
+        socket.send(| mut send | {
+            let rsp = format!("pwm Motor Esquerdo: {},\npwm Motor Direito: {}\n", motores.pwm_esq, motores.pwm_dir);
+            if send.len() < 100 {
+                return (0, "".to_string());
+            }
+            send.write(rsp.as_bytes()).unwrap();
+            (rsp.len(), rsp)
+        }).unwrap();
+    }
+    command
+}
+
 #[main]
 fn main() -> ! {
     // generator version: 0.3.1
     println!("inicio");
 
+    // inicializa os periféricos
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let mut peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
 
+    let instant = Instant::now();
+
+    // Alocador para o esp_wifi e outras funções que necessitam de alocação
     esp_alloc::heap_allocator!(size: 72 * 1024);
-
+    // Inicio da configuração do wifi
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     let esp_wifi_controller = esp_wifi::init(
         timg0.timer0,
@@ -103,8 +186,8 @@ fn main() -> ! {
     let (mut wifi_controller, mut wifi_interfaces) = esp_wifi::wifi::new(&esp_wifi_controller, peripherals.WIFI).unwrap();
 
     let wifi_config = esp_wifi::wifi::Configuration::AccessPoint(esp_wifi::wifi::AccessPointConfiguration {
-        ssid: { let mut string =  heapless::String::new(); string.push_str("robo").unwrap(); string },
-        password: { let mut string =  heapless::String::new(); string.push_str("seguidor").unwrap(); string },
+        ssid: "robo".to_string(),
+        password: "seguidor".to_string(),
         ssid_hidden: false,
         max_connections: 1,
         auth_method: WPA2Personal,
@@ -113,18 +196,17 @@ fn main() -> ! {
 
     wifi_controller.set_configuration(&wifi_config).unwrap();
     wifi_controller.start().unwrap();
-
-    // configuração smoltcp
+    // Final da configuração wifi
+    // Início configuração smoltcp
 
     let mac_address = HardwareAddress::Ethernet(EthernetAddress::from_bytes(&wifi_interfaces.ap.mac_address()));
     let ip_address = Ipv4Cidr::new(Ipv4Address::new(192, 168, 4, 1), 24);
     let ip_addr = IpCidr::Ipv4(ip_address);
-    let mut ip_addrs = [ip_address];
 
     let mut rx_buffer = [0u8; RX_BUFFER_SIZE];
     let mut tx_buffer = [0u8; TX_BUFFER_SIZE];
 
-    let mut tcp_socket = TcpSocket::new(
+    let tcp_socket = TcpSocket::new(
         SocketBuffer::new(&mut rx_buffer[..]),
         SocketBuffer::new(&mut tx_buffer[..]),
     );
@@ -138,7 +220,7 @@ fn main() -> ! {
     let mut interface = Interface::new(
         smoltcp_config,
         &mut wifi_interfaces.ap,
-        Instant::from_millis(esp_hal::time::Instant::duration_since_epoch(&esp_hal::time::Instant::now()).as_millis() as i64)
+        SmoltcpInstant::from_millis(instant.duration_since_epoch().as_millis() as i64),
     );
 
     interface.update_ip_addrs(|f| {
@@ -148,8 +230,8 @@ fn main() -> ! {
     println!("smoltcp Server: Inicializado, Ip: {:?}", interface.ipv4_addr().unwrap());
     println!("smoltcp Server: Escutando na porta 8080...");
 
-    // fim configuração smoltcp
-
+    // fim da configuração smoltcp
+    // Início da configuração do pwm para os motores
     let mut delay = Delay::new();
 
     let out_esq = peripherals.GPIO13;
@@ -166,8 +248,8 @@ fn main() -> ! {
             frequency: Rate::from_khz(PWM_FREQ_KHZ),
         }).unwrap();
 
-    let mut mot_esq: channel::Channel<'_, LowSpeed> = ledc.channel(channel::Number::Channel0, out_esq);
-    let mut mot_dir: channel::Channel<'_, LowSpeed> = ledc.channel(channel::Number::Channel1, out_dir);
+    let mut mot_esq = ledc.channel(channel::Number::Channel0, out_esq);
+    let mut mot_dir = ledc.channel(channel::Number::Channel1, out_dir);
 
     mot_esq.configure(
         channel::config::Config {
@@ -184,7 +266,8 @@ fn main() -> ! {
             pin_config: channel::config::PinConfig::PushPull
         }
     ).unwrap();
-
+    // Fim da configuração do pwm para os motores
+    // Inicio da configuração dos pinos do ADC
     let mut adc_config = adc::AdcConfig::new();
 
     let analog_pin_esq = peripherals.GPIO33;
@@ -205,55 +288,40 @@ fn main() -> ! {
     );
 
     let mut adc_sensors = adc::Adc::new(peripherals.ADC1, adc_config);
+    // Fim da configuração dos pinos do ADC
+
+    let mut last_command = Command::Stop;
+
+    let mut motores = Telemetria {
+        pwm_dir: 0,
+        pwm_esq: 0
+    };
 
     loop {
-        // smoltcp
+        let command = get_commands(&mut interface, &mut socket_set, &tcp_handle, &mut wifi_interfaces, &instant, &motores).unwrap_or(last_command);
+        last_command = command.clone();
 
-        let timestamp = Instant::from_millis(esp_hal::time::Instant::duration_since_epoch(&esp_hal::time::Instant::now()).as_millis() as i64);
-        interface.poll(timestamp, &mut wifi_interfaces.ap, &mut socket_set);
+        if command == Command::Stop {
+            mot_esq.set_duty(0).unwrap();
+            mot_dir.set_duty(0).unwrap();
+            motores.pwm_esq = 0;
+            motores.pwm_dir = 0;
+        } else if command == Command::Start {
+            let esq = nb::block!(adc_sensors.read_oneshot(&mut pin_esq)).unwrap();
+            let ctr = nb::block!(adc_sensors.read_oneshot(&mut pin_ctr)).unwrap();
+            let dir = nb::block!(adc_sensors.read_oneshot(&mut pin_dir)).unwrap();
 
-        let mut socket = socket_set.get_mut::<TcpSocket>(tcp_handle);
+            let desvio = find_deviation(esq, ctr, dir);
 
-        if !socket.is_open() {
-            socket.listen(8080).unwrap();
-            println!("socket listening");
+            let (pwm_esq, pwm_dir) = calc_pwm(desvio);
+
+            mot_esq.set_duty(pwm_esq).unwrap();
+            mot_dir.set_duty(pwm_dir).unwrap();
+
+            motores.pwm_esq = pwm_esq;
+            motores.pwm_dir = pwm_dir;
         }
-
-        if socket.may_recv() {
-            let data = socket.recv(|recv| {
-                println!("tcp:8080 recv data: {:?}", recv);
-                let mut data = recv.to_vec();
-                (data.len(), data)
-            }).unwrap();
-            let text = core::str::from_utf8(data.as_slice()).unwrap();
-            println!("tcp:8080 text: {:?}", text);
-        }
-
-        if socket.can_send() {
-            socket.abort()
-        }
-
-        // smoltcp
-
-        let esq = nb::block!(adc_sensors.read_oneshot(&mut pin_esq)).unwrap();
-        let ctr = nb::block!(adc_sensors.read_oneshot(&mut pin_ctr)).unwrap();
-        let dir = nb::block!(adc_sensors.read_oneshot(&mut pin_dir)).unwrap();
-
-        //let desvio = find_deviation(
-        // adc_sensors.read_oneshot(&mut pin_esq).unwrap(),
-        // adc_sensors.read_oneshot(&mut pin_ctr).unwrap(),
-        // adc_sensors.read_oneshot(&mut pin_dir).unwrap()
-        //);
-
-        let desvio = find_deviation(esq, ctr, dir);
-
-        let (pwm_esq, pwm_dir) = calc_pwm(desvio);
-
-        mot_esq.set_duty(pwm_esq).unwrap();
-        mot_dir.set_duty(pwm_dir).unwrap();
-
         delay.delay_millis(DELAY_LOOP_PRINCIPAL);
     }
-
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
 }
